13d73a1ef30af2c0eef589d91103caa1a9a4c3b2
Oleg-rb <oleg1107@rambler.ru>
2020-06-29 02:53:50 +0300

    Add model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   has_many :comments
+
+  validates :title, length: { maximum: 500 }, presence: true
+  validates :summary, length: { maximum: 10000 },presence: true
+  validates :body, presence: true
 end
