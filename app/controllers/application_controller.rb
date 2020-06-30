b16dd17a08faa9c777ebc2c3c0089a1050f3c3c4
Oleg-rb <oleg1107@rambler.ru>
2020-06-30 20:03:20 +0300

    Update styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :success, :danger, :info, :warning
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?    # применяем метод фильтрации только в контроллере devise
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,9 +30,10 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post, success: 'Статья успешно создана' }
         format.json { render :show, status: :created, location: @post }
       else
+        flash.now[:danger] = 'Статья не обнавлена'
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
@@ -44,9 +45,10 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post, success: 'Статья успешно обновлена' }
         format.json { render :show, status: :ok, location: @post }
       else
+        flash.now[:danger] = 'Статья не обнавлена'
         format.html { render :edit }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
@@ -58,7 +60,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_path, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_path, success: 'Статья успешно удалена' }
       # format.json { head :no_content }
     end
   end
