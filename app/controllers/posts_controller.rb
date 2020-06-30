4fc9d4a34e4b9609f517c22e9325d1cede4c8cdb
Oleg-rb <oleg1107@rambler.ru>
2020-06-30 18:00:07 +0300

    Add styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,8 +58,8 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to posts_path, notice: 'Post was successfully destroyed.' }
+      # format.json { head :no_content }
     end
   end
 
